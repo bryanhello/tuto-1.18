4345d931f2d91ac1594e97b19c112c689ec351b9
bryan <boubousautereau@gmail.com>
2022-03-29 20:52:50 +0200

    new items

--- src/main/java/fr/hellobryan/tutorialmod/TutorialMod.java
+++ src/main/java/fr/hellobryan/tutorialmod/TutorialMod.java
@@ -3,6 +3,8 @@ package fr.hellobryan.tutorialmod;
 import com.mojang.logging.LogUtils;
 import fr.hellobryan.tutorialmod.block.ModBlocks;
 import fr.hellobryan.tutorialmod.item.ModItems;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
@@ -11,6 +13,7 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
@@ -38,10 +41,15 @@ public class TutorialMod
         ModBlocks.register(eventBus);
 
         eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
         //test
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
     }
+
+    private void clientSetup(final FMLClientSetupEvent event){
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_DOOR.get(), RenderType.translucent());
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.EBONY_TRAPDOOR.get(), RenderType.translucent());}
 
     private void setup(final FMLCommonSetupEvent event)
     {
